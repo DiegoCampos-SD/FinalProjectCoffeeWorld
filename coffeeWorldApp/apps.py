293a41739b12128863f60c744339b3bd5c4aa59c
DieCamDu <dcamposd@ucenfotec.ac.cr>
2024-12-04 20:07:51 -0500

    Third Commit, name of the app changed

--- coffeeWorldApp/apps.py
+++ coffeeWorldApp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CoffeeshopappConfig(AppConfig):
+class CoffeeWWorldAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'coffeeShopApp'
+    name = 'coffeeWorldApp'
--- coffeeWorldApp/views.py
+++ coffeeWorldApp/views.py
@@ -6,11 +6,11 @@ from .forms import DrinkForm
 # Create your views here.
 """def drink_list(request):
     drinks = Drink.objects.filter(creationDate__lte=timezone.now()).order_by('creationDate')
-    return render(request, 'coffeShop/drink_list.html',{'drinks':drinks})"""
+    return render(request, 'coffeeWorld/drink_list.html',{'drinks':drinks})"""
 
 """def drink_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    return render(request, 'coffeShop/drink_detail.html', {'drink': drink})"""
+    drink = get_object_or_404(Drink, pk=pk)
+    return render(request, 'coffeeWorld/drink_detail.html', {'drink': drink})"""
 
 def drink_new(request):
     if request.method == "POST":
@@ -25,10 +25,10 @@ def drink_new(request):
     else:
         form = CreationForm()
     return ("ok")
-    #return render(request, 'coffeShop/drink_edit.html', {'form': form})
+    #return render(request, 'coffeWorld/drink_edit.html', {'form': form})
 
 """def drink_edit(request, pk):
-    post = get_object_or_404(Post, pk=pk)
+    drink = get_object_or_404(Drink, pk=pk)
     if request.method == "POST":
         form = DrinkForm(request.POST, instance=post)
         if form.is_valid():
@@ -39,4 +39,4 @@ def drink_new(request):
             return redirect('post_detail', pk=drink.pk)
     else:
         form = CreationForm(instance=post)
-    return render(request, 'coffeShop/drink_edit.html', {'form': form})"""
+    return render(request, 'coffeWorld/drink_edit.html', {'form': form})"""
