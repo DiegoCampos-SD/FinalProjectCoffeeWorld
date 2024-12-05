f0ae45d999363e71a76dc551838b97aab82d8394
AscensionXD <nhughes66@ivytech.edu>
2024-12-04 21:11:01 -0500

    finished url patterns

--- coffeeWorldApp/urls.py
+++ coffeeWorldApp/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    #path('', views.drink_list, name='drink_list'),
-    #path('coffeWorld/<int:pk>/', views.drink_detail, name='drink_detail'),
-    #path('coffeWorld/new/', views.drink_new, name='drink_new'),
-    #path('coffeWorld/<int:pk>/edit/', views.drink_edit, name='drink_edit'),
+    path('', views.drink_list, name='drink_list'),
+    path('coffeWorld/<int:pk>/', views.drink_detail, name='drink_detail'),
+    path('coffeWorld/new/', views.drink_new, name='drink_new'),
+    path('coffeWorld/<int:pk>/edit/', views.drink_edit, name='drink_edit'),
 ]
