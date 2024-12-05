d29a495ff2bad62408de24c50a4c9661b18412a4
DieCamDu <dcamposd@ucenfotec.ac.cr>
2024-12-04 23:41:28 -0500

    Forth commit, changes in the files base.html, drinks_list.html, urls, css, and views

--- coffeeWorldApp/views.py
+++ coffeeWorldApp/views.py
@@ -4,13 +4,9 @@ from .models import Drink
 from .forms import DrinkForm
 
 # Create your views here.
-"""def drink_list(request):
+def drink_list(request):
     drinks = Drink.objects.filter(creationDate__lte=timezone.now()).order_by('creationDate')
-    return render(request, 'coffeeWorld/drink_list.html',{'drinks':drinks})"""
-
-"""def drink_detail(request, pk):
-    drink = get_object_or_404(Drink, pk=pk)
-    return render(request, 'coffeeWorld/drink_detail.html', {'drink': drink})"""
+    return render(request, 'coffeeWorld/drinks_list.html',{'drinks':drinks})
 
 def drink_new(request):
     if request.method == "POST":
@@ -21,22 +17,7 @@ def drink_new(request):
             post.creationDate = timezone.now()
             post.save()
             return ("ok")
-            #return redirect('drink_detail', pk=drink.pk)
     else:
         form = CreationForm()
     return ("ok")
-    #return render(request, 'coffeeWorld/drink_edit.html', {'form': form})
-
-"""def drink_edit(request, pk):
-    drink = get_object_or_404(Drink, pk=pk)
-    if request.method == "POST":
-        form = DrinkForm(request.POST, instance=post)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.publishedDate = timezone.now()
-            post.save()
-            return redirect('post_detail', pk=drink.pk)
-    else:
-        form = CreationForm(instance=post)
-    return render(request, 'coffeeWorld/drink_edit.html', {'form': form})"""
+    
